8a6bc09efa03f4183307d7643c8c38937c24dc81
KevinYgante <k.yau.langern@gmail.com>
2024-01-24 19:51:58 -0800

    made a crow script

---
+++ Assets/Scripts/Crow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crow : MonoBehaviour
+{
+    [SerializeField] private DialogueCrow _crow;
+    [SerializeField] int crowID;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
