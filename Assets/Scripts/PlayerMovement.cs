eac6374c1927ce66ce17657152340db0ffa002a8
JorbenTheDeveloper <134649421+JorbenTheDeveloper@users.noreply.github.com>
2024-01-24 18:51:46 -0800

    added UI for active ability and brown bar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,91 +2,106 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10.5f;
-    private float originalSpeed; // To store the original speed
-    private float boostedSpeed; // Speed when boosted
-    public GameObject effectPrefab; // Assign the prefab in the editor
-    public float effectDuration = 5f; // Duration of the fear effect object
-    public GameObject effectSpawnPoint; // Assign the child GameObject here for fear effect
+    private float originalSpeed;
+    private float boostedSpeed;
+    public GameObject effectPrefab;
+    public float effectDuration = 5f;
+    public GameObject effectSpawnPoint;
 
     public float stamina = 300f;
     private float maxStamina = 300f;
-    public Slider staminaBar; // Assign this in the Unity Editor
+    public Slider staminaBar;
+    public TextMeshProUGUI staminaText;
 
-    private void Start()
-    {
-        originalSpeed = speed; // Store the original speed
-        boostedSpeed = speed * 2f; // Define boosted speed as double the original speed
-
-        // Initialize stamina bar
-        staminaBar.maxValue = maxStamina;
-        staminaBar.value = stamina;
-    }
+    public float abilityCooldown = 5f;
+    private float abilityTimer;
+    public Image abilityActiveImage; // Shown when ability is ready
+    public Image abilityCooldownImage; // Shown when ability is on cooldown
+    public TextMeshProUGUI cooldownText;
 
     [SerializeField] private GameObject _crow;
 
+    private void Start()
+    {
+        originalSpeed = speed;
+        boostedSpeed = speed * 2f;
+
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = stamina;
+
+        abilityTimer = abilityCooldown;
+    }
+
     void Update()
     {
-        // Check for left mouse button
+        HandleMovement();
+        HandleAbilityUse();
+        UpdateUI();
+        DrainStamina(1 * Time.deltaTime);  // Stamina drain of 1 per second
+    }
+
+    private void HandleMovement()
+    {
         if (Input.GetMouseButton(0) && stamina > 0)
         {
-            speed = boostedSpeed; // Boost the speed
-            DrainStamina(2 * Time.deltaTime); // Increased stamina drain when speed is boosted
+            speed = boostedSpeed;
+            DrainStamina(2 * Time.deltaTime);
         }
         else
         {
-            speed = originalSpeed; // Revert to original speed
+            speed = originalSpeed;
         }
 
-        // Movement code
         Vector3 pos = transform.position;
-
-        if (Input.GetKey("w"))
-        {
-            pos.y += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("s"))
-        {
-            pos.y -= speed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            pos.x += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            pos.x -= speed * Time.deltaTime;
-        }
+        if (Input.GetKey("w")) { pos.y += speed * Time.deltaTime; }
+        if (Input.GetKey("s")) { pos.y -= speed * Time.deltaTime; }
+        if (Input.GetKey("d")) { pos.x += speed * Time.deltaTime; }
+        if (Input.GetKey("a")) { pos.x -= speed * Time.deltaTime; }
 
         transform.position = pos;
+    }
 
-        // Ability trigger
-        if (Input.GetKeyDown(KeyCode.Space) && stamina >= 10)
+    private void HandleAbilityUse()
+    {
+        if (abilityTimer < abilityCooldown)
+        {
+            abilityTimer += Time.deltaTime;
+            abilityActiveImage.gameObject.SetActive(false);
+            abilityCooldownImage.gameObject.SetActive(true);
+
+            // Update cooldown text to show remaining time
+            cooldownText.text = "" + Mathf.RoundToInt(abilityCooldown - abilityTimer).ToString();
+        }
+        else
+        {
+            abilityActiveImage.gameObject.SetActive(true);
+            abilityCooldownImage.gameObject.SetActive(false);
+            cooldownText.text = ""; // Clear cooldown text when ability is ready
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && stamina >= 10 && abilityTimer >= abilityCooldown)
         {
             TriggerAbility();
-            DrainStamina(10); // Drain stamina when ability is used
+            DrainStamina(10);
+            abilityTimer = 0f;
         }
+    }
 
-        // Normal stamina drain
-        DrainStamina(1 * Time.deltaTime);
-
-        // Update the stamina bar
+    private void UpdateUI()
+    {
         staminaBar.value = stamina;
-
-        if (stamina == 0)
-        {
-
-        }
+        staminaText.text = "" + Mathf.RoundToInt(stamina).ToString();
     }
 
     void TriggerAbility()
     {
-        // Create the effect object at the spawn point's position
         GameObject effect = Instantiate(effectPrefab, effectSpawnPoint.transform.position, Quaternion.identity);
-        Destroy(effect, effectDuration); // Destroy the effect object after the ability duration ends
+        Destroy(effect, effectDuration);
     }
 
     private void DrainStamina(float amount)
@@ -100,7 +115,7 @@ public class PlayerMovement : MonoBehaviour
         Door door = other.GetComponent<Door>();
         if (door != null)
         {
-            door.Teleport(gameObject); // Teleport the player using the door's Teleport method
+            door.Teleport(gameObject);
         }
 
         if (other.name == "Crow")
@@ -108,7 +123,5 @@ public class PlayerMovement : MonoBehaviour
             _crow.SetActive(true);
         }
     }
-
-    
 }
 
