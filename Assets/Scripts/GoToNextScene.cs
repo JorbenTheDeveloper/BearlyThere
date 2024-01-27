e1cfa34ef0f4fe4bb2e49ba0dd37ae17ab6ca847
JorbenTheDeveloper <134649421+JorbenTheDeveloper@users.noreply.github.com>
2024-01-27 00:44:58 -0800

    fadein and out of cutscene into lvl 1

--- Assets/Scripts/GoToNextScene.cs
+++ Assets/Scripts/GoToNextScene.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GoToNextScene : MonoBehaviour
 {
+    public int cutsceneLength;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,7 @@ public class GoToNextScene : MonoBehaviour
 
     IEnumerator NextScene()
     {
-        yield return new WaitForSeconds(7);
+        yield return new WaitForSeconds(cutsceneLength);
         SceneManager.LoadScene("LevelOne");
     }
 }
